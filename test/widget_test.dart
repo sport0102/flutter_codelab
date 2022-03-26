025f81128128ff1d19d1b9532074425aa43eb8f2
Sun min Lee <sunmin.lee@pbagel.com>
2022-03-26 16:34:35 +0900

    codelab tutorial complete (Write your first Flutter app, part 1) https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1#0

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:codelab/main.dart';
+import 'package:codelab/basic_list/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
